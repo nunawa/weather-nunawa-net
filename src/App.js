1c7ec793bf29e01d2954a3ef73c77ce7e8280662
nunawa <71294849+nunawa@users.noreply.github.com>
2022-09-15 22:10:54 +0900

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import 'leaflet/dist/leaflet.css';
 import { Map } from "./Map";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Map />
+      <Navbar bg='light'>
+        <Container>
+          <Navbar.Brand href=''>全国の暑さ指数一覧</Navbar.Brand>
+          <Nav.Link className='justify-content-end'>About</Nav.Link>
+        </Container>
+      </Navbar>
+      <Container className='w-75 mt-3'>
+        <Map />
+      </Container>
     </div>
   );
 }
