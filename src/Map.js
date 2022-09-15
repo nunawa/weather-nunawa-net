2b591ae2f47bda25fce5ae7e1380414984504526
nunawa <71294849+nunawa@users.noreply.github.com>
2022-09-15 22:11:27 +0900

    Improve WBGT map to be color-coded

--- src/Map.js
+++ src/Map.js
@@ -1,22 +1,63 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet'
+import { MapContainer, TileLayer, Popup, Circle } from 'react-leaflet'
 import './Map.css';
-import Data from './amedastable.json'
+import Data from './ave_wbgt.json'
+import chroma from "chroma-js";
 
 // npm start
 
 const Circles = () => {
-    let compoList = []
+    let wbgtList = [];
 
-    for (const id in Data) {
-        const lat = Data[id]["lat"][0] + (Data[id]["lat"][1] / 60);
-        const lon = Data[id]["lon"][0] + (Data[id]["lon"][1] / 60);
-        const name = Data[id]["enName"];
+    for (const iter of Data) {
+        const aveWbgt = (Number(iter["4"]) + Number(iter["5"]) + Number(iter["6"]) + Number(iter["7"]) + Number(iter["8"]) + Number(iter["9"]) + Number(iter["10"])) / 7;
+        wbgtList.push(aveWbgt)
+    }
+
+    const limits = chroma.limits(wbgtList, "e", 6);
+    const pallet = chroma.scale(["lightskyblue", "yellow", "salmon", "crimson", "darkmagenta"]).mode("hsl").colors(7);
+
+    console.log(wbgtList);
+    console.log(pallet);
+
+    let compoList = [];
+
+    for (let i = 0; i < Data.length; i++) {
+        const lat = Number(Data[i]["lat"]);
+        const lon = Number(Data[i]["lon"]);
+        const name = Data[i]["name"];
+        const wbgt = wbgtList[i];
+        
+        let circleColor = "#FFFFF";
+        if (wbgt < limits[0]) {
+            circleColor = pallet[0];
+            
+        } else if (limits[0] <= wbgt && wbgt < limits[1]){
+            circleColor = pallet[1];
+
+        } else if (limits[1] <= wbgt && wbgt< limits[2]) {
+            circleColor = pallet[2];
+
+        } else if (limits[2] <= wbgt && wbgt < limits[3]) {
+            circleColor = pallet[3];
+
+        } else if (limits[3] <= wbgt && wbgt < limits[4]) {
+            circleColor = pallet[4];
+
+        } else if (limits[4] <= wbgt && wbgt < limits[5]) {
+            circleColor = pallet[5];
+
+        } else if (limits[5] <= wbgt) {
+            circleColor = pallet[6];
+        }
+
+        console.log(circleColor);
 
         compoList.push(
-            <Circle center={[lat, lon]} radius={3000}>
+            <Circle center={[lat, lon]} radius={3000} color={circleColor}>
                 <Popup>
-                    {name}
+                    {name}<br/>
+                    {String(Math.round(wbgt * 100)/100)}
                 </Popup>
             </Circle>
         )
@@ -29,7 +70,7 @@ const Circles = () => {
 
 export const Map = () => {
   return (
-    <MapContainer center={[35.68944,139.69167]} zoom={9} scrollWheelZoom={false}>
+    <MapContainer center={[35.68944, 139.69167]} zoom={9}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
